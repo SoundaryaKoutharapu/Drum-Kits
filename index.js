3cdbbab4f456d9026b8d488f8325ad8b0509c312
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-20 12:08:26 +0530

    added JS

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ kits.forEach(kit=>{
  
     const btnE1 = document.createElement("button");
     btnE1.classList.add("btn");
-    btnE1.style.backgroundImage = "url(images/" + kit + ".png)";
     btnE1.innerText=kit;
+    btnE1.style.backgroundImage = "url(images/" + kit + ".png)";
     containerE1.appendChild(btnE1);
-
-    audioE1.src ="Sounds/" + kit +".mp3";
     const audioE1 = document.createElement("audio");
     containerE1.appendChild(audioE1);
+    audioE1.src ="Sounds/" + kit +".mp3";
 
-})
+    btnE1.addEventListener("click", ()=>{
+        audioE1.play();
+    })
+
+    window.addEventListener("keydown", (event)=>{
+        if(event.key===kit.slice(0,1))
+        {
+            audioE1.play();
+            btnE1.style.transform = "scale(.9)";
+        };
+        setTimeout(()=>{
+          btnE1.style.transform="scale(1)"  
+        },100)
+    })
+
+});
